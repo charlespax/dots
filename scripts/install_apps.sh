06d0555858622b854ad3a548251dd8efa4a34e4a
charles.pax@gmail.com <Charles Edward Pax>
2024-04-28 10:54:29 -0400

    indented 8.

--- scripts/install_apps.sh
+++ scripts/install_apps.sh
@@ -7,17 +7,17 @@ APPLIST=''
 # Desktop Environment
 read -n1 -p "Install Sway? [y,n] " installsway
 case $installsway in
-    y|Y)
-        APPLIST+=' sway'     # sway desktop
-        APPLIST+=' swaybg'   # desktop background image tool
-        APPLIST+=' swayidle' # idle manager
-        APPLIST+=' swaylock' # screen lock
-        APPLIST+=' foot'     # terminal emulator
-        APPLIST+=' polkit'   # TODO what is polkit?
-        APPLIST+=' wofi'     # TODO application launcher ?
-        echo ' installing Sway' ;;
-    n|N) echo ' skip' ;;
-    *)   echo ' skip' ;;
+        y|Y)
+                APPLIST+=' sway'     # sway desktop
+                APPLIST+=' swaybg'   # desktop background image tool
+                APPLIST+=' swayidle' # idle manager
+                APPLIST+=' swaylock' # screen lock
+                APPLIST+=' foot'     # terminal emulator
+                APPLIST+=' polkit'   # TODO what is polkit?
+                APPLIST+=' wofi'     # TODO application launcher ?
+                echo ' installing Sway' ;;
+        n|N) echo ' skip' ;;
+        *)   echo ' skip' ;;
 esac
 
 #APPLIST+=' waybar'   # info bar at top of screen
@@ -25,46 +25,46 @@ esac
 # Fonts
 read -n1 -p "Install fonts? [y,n] " installdev
 case $installdev in
-    y|Y)
-        APPLIST+=' adobe-source-code-pro-fonts'
-        APPLIST+=' ttf-font-awesome'
-        APPLIST+=' ttf-roboto-mono'
-        echo ' installing fonts'
-        ;;
-    n|N) echo ' skip' ;;
-    *)   echo ' skip' ;;
+        y|Y)
+                APPLIST+=' adobe-source-code-pro-fonts'
+                APPLIST+=' ttf-font-awesome'
+                APPLIST+=' ttf-roboto-mono'
+                echo ' installing fonts'
+                ;;
+        n|N) echo ' skip' ;;
+        *)   echo ' skip' ;;
 esac
 
 # Development Tools
 read -n1 -p "Install dev tools? [y,n] " installdev
 case $installdev in
-    y|Y)
-        APPLIST+=' git'
-        APPLIST+=' base-devel'
-        echo ' installing dev tools'
-        ;;
-    n|N) echo ' skip' ;;
-    *)   echo ' skip' ;;
+        y|Y)
+                APPLIST+=' git'
+                APPLIST+=' base-devel'
+                echo ' installing dev tools'
+                ;;
+        n|N) echo ' skip' ;;
+        *)   echo ' skip' ;;
 esac
 
 # Extra applications
 read -n1 -p "Install extras? [y,n] " installdev
 case $installdev in
-    y|Y)
-        APPLIST+=' vim'
-        APPLIST+=' neovim'
-        APPLIST+=' neofetch'
-        APPLIST+=' tmux'
-        APPLIST+=' ctags'     # tagging tool, used with vim
-        APPLIST+=' doxygen'
-        APPLIST+=' graphviz'
-        APPLIST+=' speedcrunch' # calculator
-        APPLIST+=' acpi'        # battery information
-        APPLIST+=' iwctl'       # WiFi control
-        echo ' installing extras'
-        ;;
-    n|N) echo ' skip' ;;
-    *)   echo ' skip' ;;
+        y|Y)
+                APPLIST+=' vim'
+                APPLIST+=' neovim'
+                APPLIST+=' neofetch'
+                APPLIST+=' tmux'
+                APPLIST+=' ctags'     # tagging tool, used with vim
+                APPLIST+=' doxygen'
+                APPLIST+=' graphviz'
+                APPLIST+=' speedcrunch' # calculator
+                APPLIST+=' acpi'        # battery information
+                APPLIST+=' iwctl'       # WiFi control
+                echo ' installing extras'
+                ;;
+        n|N) echo ' skip' ;;
+        *)   echo ' skip' ;;
 esac
 
 #echo "Not installing applications..."
--- scripts/install_yay.sh
+++ scripts/install_yay.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
 
+# TODO use a temp directory
+
 git clone https://aur.archlinux.org/yay.git
 cd yay
 makepkg -si
+
+
