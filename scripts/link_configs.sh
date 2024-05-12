c7d2006fbc1626d802391908afa7225b12c1af42
Charles Edward Pax <charles.pax@gmail.com>
2024-05-12 11:28:41 -0400

    initial split of bash_profile and bashrc

--- scripts/link_configs.sh
+++ scripts/link_configs.sh
@@ -6,7 +6,7 @@
 PWD=`pwd`
 
 ln -i -s $PWD/../configs/bash_profile ~/.bash_profile
-ln -i -s $PWD/../configs/bash_profile ~/.bashrc
+ln -i -s $PWD/../configs/bashrc ~/.bashrc
 ln -i -s $PWD/../configs/foot ~/.config/
 ln -i -s $PWD/../configs/.moc ~/ # moc terminal music player
 ln -i -s $PWD/../configs/neofetch ~/.config/
