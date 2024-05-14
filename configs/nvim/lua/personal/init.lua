50733c2c02713563243ef2996f1879934e831555
charles.pax@gmail.com <Charles Edward Pax>
2024-05-14 13:49:52 -0400

    added nvim config.

---
+++ configs/nvim/lua/personal/init.lua
@@ -0,0 +1,74 @@
+require("charles.remap")
+
+-- Line Numbers
+vim.opt.number = true
+vim.opt.relativenumber = true
+vim.opt.scrolloff = 999
+-- vim.opt.cmdheight = 2
+
+-- Cursor Properties
+vim.opt.cursorline = true
+vim.opt.cursorcolumn = true
+vim.opt.colorcolumn = "80"
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
