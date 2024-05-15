5da90a06629a79bb3328d7da1ae00056e14ff9a6
charles.pax@gmail.com <Charles Edward Pax>
2024-05-15 13:20:21 -0400

    s

--- configs/nvim/lua/personal/init.lua
+++ configs/nvim/lua/personal/init.lua
@@ -1,12 +1,33 @@
 require("personal.remap")
 
 -- Line Numbers
+-------------------------------------------------------------------------------
 vim.opt.number = true
 vim.opt.relativenumber = true
-vim.opt.scrolloff = 999
--- vim.opt.cmdheight = 2
 
 -- Cursor Properties
+-------------------------------------------------------------------------------
 vim.opt.cursorline = true
 vim.opt.cursorcolumn = true
 vim.opt.colorcolumn = "80"
+vim.opt.scrolloff = 999
+
+-- Tabs
+-------------------------------------------------------------------------------
+vim.opt.expandtab = true  -- convert tabs into spaces
+--vim.opt.shiftwidth = 8
+--vim.opt.tabstop = 8
+
+-- Colors
+-------------------------------------------------------------------------------
+--vim.opt.termguicolors = true
+--vim.cmd 'colorscheme zaibatsu'
+vim.cmd 'highlight ColorColumn ctermbg=234'
+vim.cmd 'highlight CursorLine ctermbg=234'
+vim.cmd 'highlight CursorColumn ctermbg=234'
+--vim.cmd 'colorscheme torte'
+
+-- Other UI stuff
+-------------------------------------------------------------------------------
+-- vim.opt.cmdheight = 2
+vim.opt.foldcolumn = "1"
