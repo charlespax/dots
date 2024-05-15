b36ccbef2be919bb1bcf9282aac19aaaba34c36d
charles.pax@gmail.com <Charles Edward Pax>
2024-05-15 17:25:46 -0400

    s

--- configs/nvim/lua/personal/init.lua
+++ configs/nvim/lua/personal/init.lua
@@ -1,26 +1,21 @@
 require("personal.remap")
 
--- [[ Line Numbers ]]
--------------------------------------------------------------------------------
+-- [[ Line Numbers ]] ---------------------------------------------------------
 vim.opt.number = true
 vim.opt.relativenumber = true
 
--- [[ Cursor Properties ]]
--------------------------------------------------------------------------------
+-- [[ Cursor Properties ]] ----------------------------------------------------
 vim.opt.cursorline = true
 vim.opt.cursorcolumn = true
 vim.opt.colorcolumn = "80"
 vim.opt.scrolloff = 999
 
--- [[ Tabs ]]
--------------------------------------------------------------------------------
+-- [[ Tabs ]] -----------------------------------------------------------------
 vim.opt.expandtab = true  -- convert tabs into spaces
 --vim.opt.shiftwidth = 8
 --vim.opt.tabstop = 8
 
--- [[ Colors ]]
--------------------------------------------------------------------------------
---vim.opt.termguicolors = true
+-- [[ Colors ]] ---------------------------------------------------------------
 termguicolors = true
 vim.cmd 'highlight ColorColumn ctermbg=234'
 vim.cmd 'highlight CursorLine ctermbg=234'
@@ -29,16 +24,14 @@ vim.cmd 'highlight CursorColumn ctermbg=234'
 vim.cmd 'colorscheme lunaperche'
 --vim.cmd 'colorscheme slate'
 
--- [[ Other UI stuff ]]
--------------------------------------------------------------------------------
+-- [[ Other UI stuff ]] -------------------------------------------------------
 -- vim.opt.cmdheight = 2
 vim.opt.foldcolumn = "1"
 
-
--- [[ Auto Commands ]]
+-- [[ Auto Commands ]] --------------------------------------------------------
 vim.api.nvim_create_autocmd('TextYankPost', {
         desc = 'Highlight when yanking (copying) text',
-        group = vim.api.nvim_create_augroup('kickstart-highlight-yank', { clear = true }),
+        group = vim.api.nvim_create_augroup('highlight-yank', { clear = true }),
         callback = function()
                 vim.highlight.on_yank()
         end,
